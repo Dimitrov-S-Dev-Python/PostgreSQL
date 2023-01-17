865818769be405d6b3ceb07149ab3abb5f591581
Stan <dimitrov.s.dev@gmail.com>
2023-01-17 10:09:17 +0200

    DAY_1

--- 1.DAY/1.SELECT.sql
+++ 1.DAY/1.SELECT.sql
@@ -1,14 +1,22 @@
+-- Task 1
 -- See all columns in table address
 SELECT *
 FROM address
 
+-- Task 2
 -- See only columns:
 -- address, and district from Table address
 
-SELECT address, district
+SELECT
+address,
+district
 FROM address
 
+-- Task 3
 -- Get the list from customer, with first name, last name
 -- and customer's email address.
-SELECT first_name, last_name, email
+SELECT
+first_name,
+last_name,
+email
 FROM customer

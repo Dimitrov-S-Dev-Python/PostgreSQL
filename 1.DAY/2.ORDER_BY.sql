a07b03589fe474af0525d262d3d1b7aae4252f64
Stan <dimitrov.s.dev@gmail.com>
2023-01-17 18:10:58 +0200

    DAY_1

--- 1.DAY/2.ORDER_BY.sql
+++ 1.DAY/2.ORDER_BY.sql
@@ -6,9 +6,13 @@ FROM payment
 ORDER BY customer_id, amount DESC
 
 -- Task 2
+-- Select first name, last name and email and
 -- Order the customer list by last name starting from 'Z'
 -- and second criteria by first name also starting from 'Z'
+
 SELECT
-*
+first_name,
+last_name,
+email
 FROM customer
 ORDER BY last_name DESC, first_name DESC

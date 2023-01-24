0e3c202f9f4c5a8fcf69803eec3fedc22e145918
Stan <dimitrov.s.dev@gmail.com>
2023-01-25 01:12:19 +0200

    DAY_4

--- 4.DAY/Exercise.sql
+++ 4.DAY/Exercise.sql
@@ -1,3 +1,14 @@
-SELECT *
-	FROM flights
--- Task 1
+--Task 1
+-- UPPER, LOWER, LENGTH func
+-- Find customers with first and last name (lower case) with char
+-- more then 10, display email as well
+
+SELECT
+	LOWER(first_name),
+	LOWER(last_name),
+	LOWER(email)
+	FROM customer
+	WHERE LENGTH(first_name) > 10 OR LENGTH(last_name) >10
+
+--Task 2
+--LEFT, RIGHT
